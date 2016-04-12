287a347cc2a584cd0a1e4cac489a39f733b93921
dotzero <dotzero@tmtm.ru>
2016-04-12 15:22:43 +0300

    Update example runner

--- examples/cli.php
+++ examples/cli.php
@@ -1,9 +1,27 @@
+#!/usr/bin/env php
 <?php
-require '../libs/brainfuck.php';
 
-if(file_exists($argv[1]))
-{
-    $code = file_get_contents($argv[1]);
-    $bf = new Brainfuck($code, $argv[2]);
-    $bf->run();
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use \dotzero\Brainfuck;
+
+if (empty($argv[1])) {
+    printf("Using: %s [file] [input to Brainfuck script]\n", $argv[0]);
+    exit(1);
 }
+
+if (!file_exists($argv[1])) {
+    printf("Error: file %s not exists\n", $argv[1]);
+    exit(1);
+}
+
+$code = file_get_contents($argv[1]);
+$input = '';
+
+unset($argv[0], $argv[1]);
+if ($argv) {
+    $input = implode(' ', $argv);
+}
+
+$bf = new Brainfuck($code, $input);
+$bf->run();
