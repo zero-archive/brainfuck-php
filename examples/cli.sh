e803bacf13770e350c8335d115967c79e5ac4d3e
dotzero <dotzero@tmtm.ru>
2016-04-12 15:47:04 +0300

    Rename examples

--- examples/cli.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-php ./cli.php example1
-read -p "Press any key..."
-
-php ./cli.php example2 "User data "
-read -p "Press any key..."
-
-php ./cli.php example3
-read -p "Press any key..."
